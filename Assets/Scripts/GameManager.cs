f3c39d6ca192a874c2033e4236c017843d8de08b
Bryan <Bryan.Cordes@oasisdigital.com>
2022-08-08 14:21:35 -0500

    Reset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
 
@@ -20,6 +23,8 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
+        // NewGame();
         timeCounter.text = "Time: 03:00.00";
         timerGoing = false;    
         BeginTimer();
@@ -28,7 +33,7 @@ public class GameManager : MonoBehaviour
     public void BeginTimer() 
     {
         timerGoing = true;
-        elapsedTime = 2f;
+        elapsedTime =  30f;
 
         StartCoroutine(UpdateTimer());
     }
@@ -43,7 +48,9 @@ public class GameManager : MonoBehaviour
         while(timerGoing) 
         {
             if(elapsedTime <= 0){
+                // TODO: End Game and Send score
                EndTimer();
+               NewGame();
                break;
             }
             elapsedTime -= Time.deltaTime;
@@ -54,6 +61,46 @@ public class GameManager : MonoBehaviour
             yield return null;
         }
     }
-    
+
+     private int level;
+
+    private void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // private void LoadLevel(int index)
+    // {
+    //     level = index;
+
+    //     Camera camera = Camera.main;
+
+    //     // Don't render anything while loading the next scene to create
+    //     // a simple scene transition effect
+    //     if (camera != null) {
+    //         camera.cullingMask = 0;
+    //     }
+
+    //     Invoke(nameof(LoadScene), 1f);
+    // }
+
+    // private void LoadScene()
+    // {
+    //     SceneManager.LoadScene(level);
+    // }
+
+    // public void LevelComplete()
+    // {
+    //     // score += 1000;
+
+    //     int nextLevel = level;
+
+    //     if (nextLevel < SceneManager.sceneCountInBuildSettings) {
+    //         LoadLevel(nextLevel);
+    //     } else {
+    //         LoadLevel(1);
+    //     }
+    // }
+
 
 }
